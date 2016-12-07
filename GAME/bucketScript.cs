940e29a7a22f4ad69b5484cbd5da64e6c8ae07ef
Elliot Chester <tangysmelliot@gmail.com>
2016-12-07 13:20:11 +0000

    Add files via upload

---
+++ GAME/bucketScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bucketScript : MonoBehaviour {
+
+    public bool beingHeld;
+    bool onBooks;
+    GameObject BucketHolder;
+    GameObject Player;
+    bool used = false;
+
+    void Update() { 
+        if (Player == null) {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        if (beingHeld == false && onBooks == true) {
+            
+            this.GetComponent<Rigidbody2D>().gravityScale = 0;
+            this.transform.position = BucketHolder.transform.position;
+			this.GetComponent<BoxCollider2D>().enabled = false;
+            if (used == false)
+            {
+                used = true;
+
+                Player.GetComponent<PlayerController>().BucketPositioned();
+                this.gameObject.tag = "UsedBucket";
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Touched Bucket Holder");
+        if (other.gameObject.tag == "BucketHolder") {
+            BucketHolder = other.gameObject;
+            onBooks = true;
+            
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "BucketHolder")
+        {
+            BucketHolder = null;
+            onBooks = false;
+            Debug.Log("Left Bucket Holder");
+        }
+    }
+}
